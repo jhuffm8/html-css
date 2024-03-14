7911c987444758bc55af64eb8ad069bcd2f807fe
jhuffm8 <jordan.huffman85@gmail>
2024-03-14 14:31:01 -0600

    commit

---
+++ 308-intro-java/1-javascript-basics/script.js
@@ -0,0 +1,19 @@
+console.log('Hello World!')
+
+// diffrent ways to assign varibales
+
+var test = "This can be any data type: string, int, float, boolen, null";
+let mySecond = true;
+const PI = 3.141592653589793;
+
+
+
+let x =10;
+// Here x is 10
+
+{
+    let x =2;
+    // Within this block(scope), x is now 2
+}
+
+// Outside of the block, x is still 10
