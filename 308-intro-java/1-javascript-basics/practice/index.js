3c4b6255afd017099fa841a06075c5e48bfb10d6
jhuffm8 <jordan.huffman85@gmail>
2024-03-14 15:04:33 -0600

    div by 5

--- 308-intro-java/1-javascript-basics/practice/index.js
+++ 308-intro-java/1-javascript-basics/practice/index.js
@@ -8,7 +8,7 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
-
+// console.log(isSum50)
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
@@ -33,7 +33,7 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+// console.log(isValid);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
@@ -43,3 +43,11 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+
+const isDiv5 = (n1 % 5 && n2 % 5 && n3 % 5 && n4 % 5) === 0;
+
+console.log(isDiv5)
+
+
